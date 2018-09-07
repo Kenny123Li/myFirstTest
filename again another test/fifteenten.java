9994256230d3296036c22e738c78dcf83ccb165c
Kenneth Li <kmli@stu.naperville203.org>
2018-09-07 13:05:23 -0500

    almost finished the stack program

--- again another test/fifteenten.java
+++ again another test/fifteenten.java
@@ -4,28 +4,35 @@ public class fifteenten
 public static void main(String[]args)
 {
     int decision = 12;
+    Stack<Integer> driveway= new Stack<>();
+    Stack<Integer> street= new Stack<>();
     Scanner scan = new Scanner(System.in);
+    Integer in;
     while (decision != 0)
     {
         System.out.print(":");
         decision = scan.nextInt();
-        Stack<Integer> driveway;
-        Stack<Integer> street;
-        
         if (decision > 0)
         {
-            
+            driveway.push(decision);
+            System.out.print(driveway);
         }
         
         else if (decision < 0)
         {
-            
+            if (driveway.empty()){
+                 System.out.println("There are no cars in the driveway.");}
+            while (driveway.peek() != (-decision)){
+                street.push(driveway.pop());
+                System.out.println("Street: "+street);
+                System.out.println("Driveway: "+driveway);
+            }
+            driveway.pop();
+            while (!street.empty()){
+            driveway.push(street.pop());
+            System.out.println("Street: "+street);
+            System.out.println("Driveway: "+driveway);}
         }
-        
-        else
-        {
-        }
-        
     }
 }
 }
