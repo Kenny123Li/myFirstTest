b5d86770e7e3a6138b5898b2db44c4b14f8ef2a9
Kenneth Li <kmli@stu.naperville203.org>
2018-09-07 11:59:36 -0500

    Added a java file that prints something

---
+++ again another test/practice.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class practice here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class practice
+{
+public static void main(String[] args)
+{
+    System.out.println("I have to commit this to git");
+}
+}
