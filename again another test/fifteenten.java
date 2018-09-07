93ebca2953252135119bdad41d949a8ce29264f7
Kenneth Li <kmli@stu.naperville203.org>
2018-09-07 12:18:44 -0500

    started program fifteen ten

---
+++ again another test/fifteenten.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class fifteenten
+{
+public static void main(String[]args)
+{
+    int decision = 12;
+    Scanner scan = new Scanner(System.in);
+    while (decision != 0)
+    {
+        System.out.print(":");
+        decision = scan.nextInt();
+        Stack<Integer> driveway;
+        Stack<Integer> street;
+        
+        if (decision > 0)
+        {
+            
+        }
+        
+        else if (decision < 0)
+        {
+            
+        }
+        
+        else
+        {
+        }
+        
+    }
+}
+}
